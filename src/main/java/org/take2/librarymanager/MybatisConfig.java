49b71676b30d9b9095cf56666505a6a7f93db12a
Travelerext <2970111877@qq.com>
2025-06-23 12:41:42 +0800

    使用ipage\

---
+++ src/main/java/org/take2/librarymanager/MybatisConfig.java
@@ -0,0 +1,18 @@
+package org.take2.librarymanager;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MybatisConfig {
+
+    @Bean
+    public MybatisPlusInterceptor interceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL)); // 根据你的数据库选择
+        return interceptor;
+    }
+}
